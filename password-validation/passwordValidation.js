820645651067b982ff66783229581df5bb6b5c26
Sergio Di Martino <sergio@webdevserg.io>
2019-09-08 21:16:49 -0700

    Adds Password Validation exercise solution

---
+++ password-validation/passwordValidation.js
@@ -0,0 +1,28 @@
+var inquirer = require("inquirer");
+
+console.log("");
+console.log("Password Validator");
+console.log("------------------");
+console.log("");
+
+inquirer
+  .prompt([
+    {
+      type: "string",
+      name: "inputPassword",
+      message: "Please enter your password:"
+    }
+  ])
+  .then(answers => {
+    console.log("");
+
+    const password = answers.inputPassword;
+
+    if (password === "123456") {
+      console.log("Welcome!");
+    } else {
+      console.log("I don't know you.");
+    }
+
+    console.log("");
+  });
